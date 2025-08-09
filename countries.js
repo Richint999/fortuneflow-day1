0705a1b1a7c375c032ba3ba429af6ebfc63d354e
Richint999 <xw@xuweis.com>
2025-08-09 11:41:23 -0700

    Add files via upload

---
+++ countries.js
@@ -0,0 +1,10 @@
+const COUNTRIES = [
+  { value: 'CN', label: 'China (UTC+8)' },
+  { value: 'US', label: 'United States (UTC−5 to UTC−10)' },
+  { value: 'GB', label: 'United Kingdom (UTC+0/UTC+1)' },
+  { value: 'CA', label: 'Canada (UTC−3.5 to UTC−8)' },
+  { value: 'AU', label: 'Australia (UTC+8 to UTC+11)' },
+  { value: 'DE', label: 'Germany (UTC+1/UTC+2)' },
+  { value: 'FR', label: 'France (UTC+1/UTC+2)' },
+  { value: 'ES', label: 'Spain (UTC+1/UTC+2)' }
+];
